549e2c0072528f811c76e948b6405e8437714813
hieunguy02 <134108393+hieunguy02@users.noreply.github.com>
2023-09-22 17:36:38 -0700

    Add Logo, Fix Navigation Bar Color, Change Color Font of Home Button

--- FridgeFriend/FridgeFriendApp/apps.py
+++ FridgeFriend/FridgeFriendApp/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class FridgefriendappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'FridgeFriend'
+    name = 'FridgeFriendApp'
--- FridgeFriend/FridgeFriendApp/models.py
+++ FridgeFriend/FridgeFriendApp/models.py
@@ -1,3 +1,59 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=255)
+    create_at = models.DateTimeField(auto_now_add=True)
+    
+class Fridge(models.Model):
+    fridge_name = models.CharField(max_length=255)
+    create_at = models.DateTimeField(auto_now_add=True)
+    last_update = models.DateTimeField(auto_now=True)
+
+
+class UserFridge(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    fridge = models.ForeignKey(Fridge, on_delete=models.CASCADE)
+    
+class Category(models.Model):
+    DAIRY = 'D'
+    FRUIT = 'F'
+    VEGETABLE = 'V'
+    MEAT = 'M'
+    EGG = 'E'
+    SEAFOOD = 'S'
+    COOKED_FOOD = 'CF'
+    MANUFACTURE_PRODUCT = 'MP'
+    OTHERS = 'O'
+    
+    CATEGORY_CHOICES = [
+       (DAIRY, 'Dairy'),
+       (FRUIT, 'Fruit'),
+       (VEGETABLE, 'Vegetable'),
+       (MEAT, 'Meat'),
+       (EGG, 'Egg'),
+       (SEAFOOD, 'Seafood'),
+       (COOKED_FOOD, 'Cooked food'),
+       (MANUFACTURE_PRODUCT, 'Manufacture Profuct'),
+       (OTHERS, 'Others'),
+    ]
+    
+    category_name = models.CharField(max_length=26, choices=CATEGORY_CHOICES, default='Others')
+    days_last = models.SmallIntegerField()
+    
+class Item(models.Model):
+    item_name = models.CharField(max_length=255)
+    quantity = models.PositiveSmallIntegerField()
+    expiry_date = models.DateField(null=True)
+    fridge = models.ForeignKey(Fridge, on_delete=models.PROTECT)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+
+class ItemStatus(models.Model):
+    item = models.OneToOneField(Item, on_delete=models.CASCADE)
+    freeze = models.BooleanField()
+    create_at = models.DateTimeField(auto_now_add=True)
+    
+    
+    
