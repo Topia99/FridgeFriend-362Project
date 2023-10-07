42be74cad580d6b849cf89134312815aaf632bd1
Jason Zeng <98057042+Topia99@users.noreply.github.com>
2023-10-06 18:49:00 -0700

    Revert "Create 'core' app for user authentication"
    This reverts commit 5d1b50f200b6568b3d5120e598c73873abaa00e5.

--- FridgeFriend/core/models.py
+++
@@ -1,6 +0,0 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-
-# Create a Abstract User model here
-class User(AbstractUser):
-    email = models.EmailField(unique=True)  # User's email has to be unique
