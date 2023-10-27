582de81a0382913d6df1e3917ee2201332772dc9
Topia99 <1242911982z@gmail.com>
2023-10-26 19:04:58 -0700

    back to the last version

--- FridgeFriend/FridgeFriendApp/urls.py
+++ FridgeFriend/FridgeFriendApp/urls.py
@@ -2,19 +2,19 @@ from django.urls import path, include
 from rest_framework import routers
 from . import views
 
-router = routers.DefaultRouter()
-router.register('', views.ItemViewSet)
-
-urlpatterns = [
-    path('', include(router.urls)),
-]
-
-
+# router = routers.DefaultRouter()
+# router.register('', views.ItemViewSet)
 
 # urlpatterns = [
-#     path("", views.index, name="index"),
-#     path("login/", views.signin),
-#     path("index/addrecord/", views.addrecord, name="addrecord"),
-#     path("index/deleterecord/<int:id>", views.deleterecord, name="deleterecord"),
-#     path("index/updaterecord/<int:id", views.updaterecord, name="updaterecord")
+#     path('', include(router.urls)),
 # ]
+
+
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("login/", views.signin),
+    path("index/addrecord/", views.addrecord, name="addrecord"),
+    path("index/deleterecord/<int:id>", views.deleterecord, name="deleterecord"),
+    path("index/updaterecord/<int:id", views.updaterecord, name="updaterecord")
+]
--- FridgeFriend/FridgeFriendApp/views.py
+++ FridgeFriend/FridgeFriendApp/views.py
@@ -13,45 +13,45 @@ from django.shortcuts import render
 import requests
 
 
-# # Create your views here.
-# def index(request):
-#     template = loader.get_template('main.html')
-#     item_list = Item.objects.all()
-#     context = {
-#         "item_list":item_list
-#     }
-#     return HttpResponse(template.render(context))
+# Create your views here.
+def index(request):
+    template = loader.get_template('main.html')
+    item_list = Item.objects.all()
+    context = {
+        "items":item_list
+    }
+    return HttpResponse(template.render(context))
 
-# def signin(request):
-#     return render(request, 'signin.html')
+def signin(request):
+    return render(request, 'signin.html')
 
-# @csrf_exempt #let's revist this after the login page is made
-# def addrecord(request):
-#     name = request.POST['item']
-#     exp_date = request.POST['expdate']
-#     current_fridge = Fridge.objects.first()
-#     category_choice = Category.objects
+@csrf_exempt #let's revist this after the login page is made
+def addrecord(request):
+    name = request.POST['item']
+    exp_date = request.POST['expdate']
+    current_fridge = Fridge.objects.first()
+    category_choice = Category.objects
 
-#     item = Item(item_name=name, expiry_date=exp_date,
-#                 quantity=1, fridge_id=1, category_id=1)
-#     item.save()
-#     return HttpResponseRedirect(reverse('index'))
+    item = Item(item_name=name, expiry_date=exp_date,
+                quantity=1, fridge_id=1, category_id=1)
+    item.save()
+    return HttpResponseRedirect(reverse('index'))
 
-# def deleterecord(request, id):
-#     item_  = Item.objects.get(id=id)
-#     item_.delete()
-#     return HttpResponseRedirect(reverse('index'))
+def deleterecord(request, id):
+    item_  = Item.objects.get(id=id)
+    item_.delete()
+    return HttpResponseRedirect(reverse('index'))
 
-# @csrf_exempt
-# def updaterecord(request, id):
-#     item_ = Item.objects.get(id=id)
-#     name = request.POST['item']
-#     exp_date = request.POST['expdate']
+@csrf_exempt
+def updaterecord(request, id):
+    item_ = Item.objects.get(id=id)
+    name = request.POST['item']
+    exp_date = request.POST['expdate']
 
-#     item_.item_name = name
-#     item_.expiry_date = exp_date
-#     item_.save()
-#     return HttpResponseRedirect(reverse('index'))
+    item_.item_name = name
+    item_.expiry_date = exp_date
+    item_.save()
+    return HttpResponseRedirect(reverse('index'))
 
 # from rest_framework.renderers import TemplateHTMLRenderer
 
@@ -61,20 +61,20 @@ import requests
 #     context = {'users': serializer.data}
 #     return render(request, 'index.html', context=context, renderer=TemplateHTMLRenderer())
 
-class ItemViewSet(ModelViewSet):
-    queryset = Item.objects.all()
-    serializer_class = ItemSerializer
+# class ItemViewSet(ModelViewSet):
+#     queryset = Item.objects.all()
+#     serializer_class = ItemSerializer
     
-    # template_name = 'main.html'
-    renderer_classes = (JSONRenderer, TemplateHTMLRenderer)
+#     # template_name = 'main.html'
+#     renderer_classes = (JSONRenderer, TemplateHTMLRenderer)
     
 
 
-    def list(self, request, *args, **kwargs):
-        response = super(ItemViewSet, self).list(request, *args, **kwargs)
-        if request.accepted_renderer.format == 'html':
-            return Response({'items': response.data}, template_name='main.html')
-        return response
+#     def list(self, request, *args, **kwargs):
+#         response = super(ItemViewSet, self).list(request, *args, **kwargs)
+#         if request.accepted_renderer.format == 'html':
+#             return Response({'items': response.data}, template_name='main.html')
+#         return response
     
     # def create(self, request, *args, **kwargs):
         
