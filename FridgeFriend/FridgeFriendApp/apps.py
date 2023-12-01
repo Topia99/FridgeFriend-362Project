697717983f2fab700e79ab79ecfc1c40f499e4ec
Casey Abrego <73695513+Dualium@users.noreply.github.com>
2023-12-01 03:25:58 -0800

    added mailgun support for expiring items

--- FridgeFriend/FridgeFriendApp/apps.py
+++ FridgeFriend/FridgeFriendApp/apps.py
@@ -1,6 +1,12 @@
 from django.apps import AppConfig
+import os
 
 
 class FridgefriendappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'FridgeFriendApp'
+
+    def ready(self):
+        from . import jobs
+        if os.environ.get('RUN_MAIN', None) != 'true':
+            jobs.start_scheduler()
---
+++ FridgeFriend/FridgeFriendApp/jobs.py
@@ -0,0 +1,41 @@
+from schedule import Scheduler
+import time
+import datetime
+from .models import Item, Fridge
+import requests
+from . import secret_keys
+
+def send_warning_email(item):
+    user_email = Fridge.objects.filter(id=item.fridge_id)[0].users.first().email
+   
+    return requests.post(
+		"https://api.mailgun.net/v3/fridgefriendapp.com/messages",
+		auth=("api", secret_keys.get_mailgun_key()),
+		data={"from": "System <system@fridgefriendapp.com>",
+			"to": ["" + user_email],
+			"subject": "Food Expiration Notice: " + item.item_name,
+			"text": "You have some food that's expiring soon! Please check on your " + item.item_name})
+        
+def check_date():
+    try:
+        for item in Item.objects.all():
+            currentDate = datetime.datetime.now()
+            expiryDate = datetime.datetime.combine(item.expiry_date, datetime.datetime.min.time())
+            timeLeft = currentDate - expiryDate
+            timeCutOff = datetime.timedelta(hours=24)
+            if timeLeft < timeCutOff:
+                print("Mailgun response: " + send_warning_email(item))
+    except Exception as e:
+        print(e)
+        time.sleep(100)
+    time.sleep(1000)
+
+
+def start_scheduler():
+    scheduler = Scheduler()
+    scheduler.every(5).seconds.do(check_date)
+
+    active = True
+    while active:
+        scheduler.run_pending()
+        time.sleep(1)
